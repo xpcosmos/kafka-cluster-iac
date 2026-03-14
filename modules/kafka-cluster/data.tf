06f5731e54e7619d48b5536068635862e87d5cc0
xpcosmos <mikeias.d.s.o@gmail.com>
2026-03-14 14:00:53 -0300

    refactor:

--- modules/kafka-cluster/data.tf
+++ modules/kafka-cluster/data.tf
@@ -5,13 +5,13 @@ data "cloudinit_config" "foobar" {
   base64_encode = false
   for_each      = local.brokers
 
-  part { # TODO Verificar init.sh
+  part {
     filename     = "init.sh"
     content_type = "text/x-shellscript"
     content      = file("${path.module}/scripts/init.sh")
   }
 
-  part { # TODO Verificar install-kafka.sh
+  part {
     filename     = "install-kafka.sh"
     content_type = "text/x-shellscript"
     content = templatefile("${path.module}/scripts/install-kafka.sh",
@@ -21,14 +21,13 @@ data "cloudinit_config" "foobar" {
       }
     )
   }
-  part { # TODO Verificar setup-kafka-storage.sh
+  part {
     filename     = "setup-kafka-storage.sh"
     content_type = "text/x-shellscript"
     content = templatefile("${path.module}/scripts/format-kafka-storage.sh",
       {
         kafka_home          = var.kafka_home
-        bootstrap_servers   = join(",", local.bootstrap_servers)
-        initial_controllers = join(",", local.initial_controllers)
+        initial_controllers = local.initial_controllers
         cluster_id          = random_uuid.kafka_cluster_id.id
       }
     )
@@ -39,9 +38,9 @@ data "cloudinit_config" "foobar" {
     content_type = "text/x-shellscript"
     content = templatefile("${path.module}/scripts/setup-prometheus.sh",
       {
-        prometheus_dirname = dirname(local.prometheus_properties_filename)
+        prometheus_dirname             = dirname(local.prometheus_properties_filename)
         prometheus_properties_filename = local.prometheus_properties_filename
-        prometheus_properties_content      = file("${path.module}/prometheus/kafka_config.yml")
+        prometheus_properties_content  = file("${path.module}/prometheus/kafka_config.yml")
       }
     )
   }
@@ -51,10 +50,10 @@ data "cloudinit_config" "foobar" {
     content_type = "text/x-shellscript"
     content = templatefile("${path.module}/scripts/setup-kafka-redis-connect.sh",
       {
-        redis_sink_properties_filename    = local.redis_sink_connect_filename
-        redis_sink_properties_content = templatefile("${path.module}/properties/redis-sink.properties.tfpl", merge(var.redis_sink, { topics = var.topics }))
-        connector_properties_filename     = local.connector_properties_filename
-        connector_properties_content  = templatefile("${path.module}/properties/redis-sink.properties.tfpl", merge(var.redis_sink, { topics = var.topics }))
+        redis_sink_properties_filename = local.redis_sink_connect_filename
+        redis_sink_properties_content  = templatefile("${path.module}/properties/redis-sink.properties.tfpl", merge(var.redis_sink, { topics = var.topics }))
+        connector_properties_filename  = local.connector_properties_filename
+        connector_properties_content   = templatefile("${path.module}/properties/redis-sink.properties.tfpl", merge(var.redis_sink, { topics = var.topics }))
       }
     )
   }
--- modules/kafka-cluster/scripts/setup-kafka-storage.sh
+++ modules/kafka-cluster/scripts/setup-kafka-storage.sh
@@ -2,5 +2,5 @@ sudo ${kafka_home}/bin/kafka-storage.sh \
   format \
   -t ${ cluster_id }\
   -c ${ kafka_home }/config/server.properties\
-  --initial-controllers ${ initial_controllers }
+  --initial-controllers ${ join(",", initial_controllers) }
 
