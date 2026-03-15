88a2daf55da60464668d8c4121f8d3b05c1b39f6
xpcosmos <mikeias.d.s.o@gmail.com>
2026-03-14 23:16:01 -0300

    refactor: movido network de compute engine

--- compute_engine.tf
+++ compute_engine.tf
@@ -180,24 +180,3 @@ resource "google_compute_instance" "grafana" {
   }
 
 }
-
-resource "google_compute_address" "external_ip_address" {
-  name         = "external-ip-static"
-  address_type = "EXTERNAL"
-  network_tier = "PREMIUM"
-}
-
-output "instance_external_ip" {
-  value       = google_compute_address.external_ip_address.address
-  description = "The static external IP address of the VM instance"
-}
-
-resource "google_compute_address" "external_ip_address_grafana" {
-  name         = "external-ip-static-grafana"
-  address_type = "EXTERNAL"
-  network_tier = "PREMIUM"
-}
-output "external_ip_address_grafana" {
-  value       = google_compute_address.external_ip_address_grafana.address
-  description = "The static external IP address of the VM instance"
-}
